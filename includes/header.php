313e768e6843ccb2a8d5bc4fe38abf27ebcbd525
unknown <jhen.1881@gmail.com>
2022-10-18 15:44:01 -0400

    inicio

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/header.css">
+    <title>Registro de Equipos</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="img/logo.png" alt="logo dep">
+            <h2 class="nombre">SISTEMA DE REGISTROS Y MANTENIMIENTOS
+            I.G.B.J.</h2>
+        </div>
+    </header>
+    
+
+<nav class= "navbar navbar-dark bg-dark">
+    <div class= "container">
+        <a href="registro.php" class="navbar-brand"> Registro </a>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include("database.php")?>
+<?php include("includes/header.php")?>
+<link rel="stylesheet" href="styles">
+
+
+   
+<?php include("includes/footer.php")?>
